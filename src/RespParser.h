e5e896f5a19d81b77ecc71a49e81e59fd7251838
owaysmb <owaysmb45@gmail.com>
2026-07-05 16:21:01 +0300

    codecrafters submit [skip ci]

---
+++ src/RespParser.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <thread>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+
+
+vector<string> RESP_parse(const string &message)
+{
+
+  vector<string> result;
+  int pos = 0;
+
+  if (message[pos] != '*')
+    return result;
+
+  pos++;
+
+  int ElementsN = stoi(message.substr(pos, message.find("\r\n", pos) - pos));
+  pos = message.find("\r\n", pos) + 2;
+
+  for (int i = 0; i < ElementsN; i++)
+  {
+    if (message[pos] != '$')
+      break;
+    pos++;
+
+    int newlength = stoi(message.substr(pos, message.find("\r\n", pos) - pos));
+    pos = message.find("\r\n", pos) + 2;
+
+    string word = message.substr(pos, newlength);
+    result.push_back(word);
+
+    pos += newlength + 2;
+  }
+  return result;
+}
---
+++ src/Storage.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <thread>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+
+
+using namespace std;
+bool Multi = false;
+bool isNumber(const string &str)
+{
+  if (str.empty())
+    return false;
+  return str.find_first_not_of("0123456789") == std::string::npos;
+}
+class ListStorage{
+private:
+  unordered_map<string, chrono::steady_clock::time_point> ExpiryTimes;
+  unordered_map<string, string> Database;
+  unordered_map<string, vector<string>> List;
+  map<string, vector<pair<string, map<string, string>>>> Streams;
+  mutex mtx;
+  condition_variable cv;
+  vector<vector<string>> Q;
+  int ExecCounts = 0;
+
+public:
+  void handlePing(vector<string> &cmd, int client_fd);
+  void handleEcho(vector<string> &cmd, int client_fd);
+  void handleSET(vector<string> &cmd, int client_fd);
+  void handleGET(vector<string> &cmd, int client_fd);
+  void handleRPUSH(vector<string> &cmd, int client_fd);
+  void handleLPUSH(vector<string> &cmd, int client_fd);
+  void handleLRANGE(vector<string> &cmd, int client_fd);
+  void handleLLEN(vector<string> &cmd, int client_fd);
+  void handleLPOP(vector<string> &cmd, int client_fd);
+  void handleBLPOP(vector<string> &cmd, int client_fd);
+  void handleTYPE(vector<string> &cmd, int client_fd);
+  void handleXADD(vector<string> &cmd, int client_fd);
+  void handleXRANGE(vector<string> &cmd, int client_fd);
+  void handleXREAD(vector<string> &cmd, int client_fd);
+  void handleXREAD_BLOCK(vector<string> &cmd, int client_fd);
+  void handleINCR(vector<string> &cmd, int client_fd);
+  void handleQueuing(vector<string> &cmd, int client_fd);
+  void dispatch(vector<string> &cmd, int client_fd);
+  
+
+};
