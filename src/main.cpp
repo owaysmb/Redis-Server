9461ae996f151b7e61064cca59421c93de5cd841
owaysmb <owaysmb45@gmail.com>
2026-07-05 15:29:25 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -749,7 +749,10 @@ public:
     {
       for (int i = 0; i < v.size(); i++)
       {
-        if (v[0] == "GET") handleGET(v, client_fd);
+        if (v[0] == "GET") {
+          const char *nullReply = "$-1\r\n";
+          send(client_fd, nullReply, strlen(nullReply), 0);
+        }
       }
       
       
