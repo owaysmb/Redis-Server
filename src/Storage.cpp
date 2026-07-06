daa4da6aa4fb9ee2f8fa3ce698f8db0d96ab9f39
owaysmb <owaysmb45@gmail.com>
2026-07-06 18:07:30 +0300

    codecrafters submit [skip ci]

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -35,7 +35,7 @@ void ListStorage::dispatch(vector<string> &cmd, int client_fd)
     if (cmd.empty())
         return;
 
-    if (clients[client_fd].multi && cmd[0] != "EXEC" && cmd[0] != "MULTI")
+    if (clients[client_fd].multi && cmd[0] != "EXEC" && cmd[0] != "MULTI" && cmd[0] != "DISCARD")
     {
         handleQueuing(cmd, client_fd);
         return;
