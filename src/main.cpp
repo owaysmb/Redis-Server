f104d62957dc8c5a907b3b8ac59f9806e83e3622
owaysmb <owaysmb45@gmail.com>
2026-07-15 20:15:04 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -98,86 +98,78 @@ void connectToMaster(const string &masterHost, const string &masterPort)
 
 int main(int argc, char *argv[])
 {
-
   cout << unitbuf;
   cerr << unitbuf;
   string port = "6379";
-  string masterHost , masterPort;
-  if (argc == 5)
-  {
-    masterHost = argv[3];
-    masterPort = argv[4];
-  }
+  string masterHost, masterPort;
 
   for (int i = 1; i < argc; i++)
   {
-
     if (string(argv[i]) == "--port" && i + 1 < argc)
     {
       port = argv[i + 1];
     }
-    else if (string(argv[i]) == "--replicaof")
+    else if (string(argv[i]) == "--replicaof" && i + 1 < argc)
     {
       isMaster = false;
+      string replicaofArg = argv[i + 1];
+      size_t spacePos = replicaofArg.find(' ');
+      masterHost = replicaofArg.substr(0, spacePos);
+      masterPort = replicaofArg.substr(spacePos + 1);
+      i++;
     }
   }
 
-  // int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0)
+  {
+    cerr << "Failed to create server socket\n";
+    return 1;
+  }
 
-  // if (server_fd < 0)
-  // {
-  //   cerr << "Failed to create server socket\n";
-  //   return 1;
-  // }
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+  {
+    cerr << "setsockopt failed\n";
+    return 1;
+  }
 
-  // int reuse = 1;
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(stoi(port));
 
-  // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-  // {
-  //   cerr << "setsockopt failed\n";
-  //   return 1;
-  // }
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+  {
+    cerr << "Failed to bind to port " << port << "\n";
+    return 1;
+  }
 
-  // struct sockaddr_in server_addr;
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0)
+  {
+    cerr << "listen failed\n";
+    return 1;
+  }
 
-  // server_addr.sin_family = AF_INET;
-  // server_addr.sin_addr.s_addr = INADDR_ANY;
-  // server_addr.sin_port = htons(stoi(port));
+  // Do the handshake with the master ONCE, before accepting client connections
+  if (!isMaster)
+  {
+    thread(connectToMaster, masterHost, masterPort).detach();
+  }
 
-  // if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
-  // {
-  //   cerr << "Failed to bind to port " << port << "\n";
-  //   return 1;
-  // }
-
-  // int connection_backlog = 5;
-
-  // if (listen(server_fd, connection_backlog) != 0)
-  // {
-  //   cerr << "listen failed\n";
-  //   return 1;
-  // }
-
-  // struct sockaddr_in client_addr;
-  // int client_addr_len = sizeof(client_addr);
-  // cout << "Waiting for a client to connect...\n";
-
-  // cout << "Logs from your program will appear here!\n";
-
-  // cout << "Client connected\n";
+  struct sockaddr_in client_addr;
+  int client_addr_len = sizeof(client_addr);
+  cout << "Waiting for a client to connect...\n";
+  cout << "Logs from your program will appear here!\n";
 
   while (true)
   {
-    // int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-    // thread t(handleCLient, client_fd);
-    // t.detach();
-    if (!isMaster)
-    {
-      thread(connectToMaster, masterHost, masterPort).detach();
-    }
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+    thread t(handleCLient, client_fd);
+    t.detach();
   }
 
-  // close(server_fd);
-
+  close(server_fd);
   return 0;
 }
